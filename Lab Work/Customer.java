15a73c6fb05fc6334708d9d742ffa03572bc20c6
hellomeruert <39824103+hellomeruert@users.noreply.github.com>
2019-02-20 18:34:02 +0600

    Update Customer.java

--- Lab Work/Customer.java
+++ Lab Work/Customer.java
@@ -1,9 +1,25 @@
 package soft;
 
+import java.util.HashSet;
+import java.util.Vector;
+
 public class Customer {
     private ShoppingCart cart;
     private String name;
     private Store store;
+
+    {
+        cart = new ShoppingCart();
+    }
+
+    public Customer()
+    {
+
+    }
+    public Customer(String name){
+        this.name = name;
+        store = null;
+    }
 
     public String getName() {
         return name;
@@ -33,4 +49,25 @@ public class Customer {
     public String toString() {
         return name +" "+ cart.toString() +" "+ store.toString();
     }
+
+    public void addItem(Item i) {
+        cart.addItem(i);
+    }
+    public void showItems(){
+
+        if(cart.getItems().isEmpty())
+        {
+            System.out.println("There are no any items in your shopping cart");
+            return;
+        }
+        int pos = 0;
+        double total = 0;
+        for(Item i: cart.getItems()){
+            System.out.printf("[%d] - %s %s %s \n", ++pos, i.getId(), i.getName(), i.getPrice());
+            total += i.getPrice();
+        }
+
+        System.out.println("Total: " + total + " tenge");
+    }
+
 }
