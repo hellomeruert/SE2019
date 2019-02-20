f2d21462e5d059aa4c05ed7abd383c188123d0c0
hellomeruert <39824103+hellomeruert@users.noreply.github.com>
2019-02-20 18:37:01 +0600

    Update ShoeStore.java

--- Lab Work/ShoeStore.java
+++ Lab Work/ShoeStore.java
@@ -1,19 +1,67 @@
-package soft;
+ackage soft;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class ShoeStore extends Store {
-    @Override
-    public void enter(Customer c) {
-        if(c.getCart()!=null) {
-            ArrayList<Customer> cust = this.getCustomers();
-            cust.add(c);
-            this.setCustomers(cust);
-        }else{
-            super.addObserver((Observer)c);
-        }
 
+    public void enter(Customer c) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(String.format("You're welcome to %s, %s", getName(), c.getName()));
+        while(true)
+        {
+            if(items().isEmpty())
+            {
+                System.out.println(getName() + " is out of stock");
+                return;
+            }
+            int choice = 0;
+            int pos=1;
+            String msg = "";
+            do{
+                System.out.println(msg);
+                for(Item i: items())
+                {
+                    System.out.println(String.format("[%d] - %s _____ %f tenge", pos++, i.getName(), i.getPrice()));
+                }
+
+                System.out.printf("[%d] - Shopping cart\n", pos);
+                System.out.printf("[%d] - Exit\n", pos+1);
+
+                choice = in.nextInt();
+                msg = "Wrong input format, try again\n";
+            }
+            while(choice<1 || choice>pos+1);
+
+            if(choice == pos){
+                c.showItems();
+            }
+            else if(choice == pos+1){
+                return;
+            }
+            else
+            {
+                Item cur = items().get(choice-1);
+                msg = "";
+                do {
+                    System.out.println(msg);
+                    System.out.printf("%s, %f tenge\n\n", cur.getName(), cur.getPrice());
+                    System.out.println("[1] - Buy");
+                    System.out.println("[2] - Back");
+
+                    choice = in.nextInt();
+                    msg = "Wrong input format, try again\n";
+                }
+                while (choice < 1 || choice > 2);
+
+                if(choice == 1)
+                {
+                    c.addItem(cur);
+                    items().remove(cur);
+                }
+            }
+        }
     }
 
     @Override
@@ -22,17 +70,6 @@ public class ShoeStore extends Store {
             ArrayList<Customer> cust = this.getCustomers();
             cust.remove(c);
             this.setCustomers(cust);
-            HashMap<Item, Integer> purchase=c.getCart().getItems();
-            HashMap<Item, Integer> allItems=super.getItems();
-
-            for (Item i:purchase.keySet()
-                 ) {
-                allItems.replace(i,allItems.get(i),allItems.get(i)-purchase.get(i));
-            }
-            setItems(allItems);
-            c.setCart(new ShoppingCart());
-
-
         }else{
             ArrayList<Observer> o=getObservers();
             o.remove((Observer)c);
